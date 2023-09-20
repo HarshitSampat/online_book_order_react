d10bb62797b7d17dc3a0b076f5aed0584e4ff1ee
HarshitSampat <60622363+HarshitSampat@users.noreply.github.com>
2023-09-20 15:04:05 +0530

    add crossOrigin in src fr image

--- src/components/BookItem.tsx
+++ src/components/BookItem.tsx
@@ -6,21 +6,22 @@ interface BookItemProps {
   onAddToCartClick: (book: Book) => void;
 }
 
-const BookItem: React.FC<BookItemProps> = ({ book,onAddToCartClick  }) => {
-
-    const handleAddToCartClick = ()=>{
-        console.log("Funcitn called");
-        onAddToCartClick(book);
-    }
+const BookItem: React.FC<BookItemProps> = ({ book, onAddToCartClick }) => {
+  const handleAddToCartClick = () => {
+    console.log('Funcitn called');
+    onAddToCartClick(book);
+  };
 
   return (
     <div className="book-item mt-4">
-      <img src={book.image} alt={book.title} height={300}/>
+      <img src={book.image }crossOrigin="anonymous" alt={book.title} height={300} />
       <h3>{book.title}</h3>
       <p>Author: {book.author}</p>
       <p>Price: ${book.price.toFixed(2)}</p>
       <p>Stock: {book.stock}</p>
-      <button className='btn btn-dark' onClick={handleAddToCartClick}>Add to cart</button>
+      <button className="btn btn-dark" onClick={handleAddToCartClick}>
+        Add to cart
+      </button>
     </div>
   );
 };
